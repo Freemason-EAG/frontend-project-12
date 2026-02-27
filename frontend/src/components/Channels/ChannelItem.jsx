5928f3a60b8ea3021ab818a73a35c9a95b07938b
Freemason-EAG <egoyan.arutyun@mail.ru>
2026-02-27 14:42:54 +0300

    Move channel name filtering logic from AddChannelModal to ChannelItem

--- frontend/src/components/Channels/ChannelItem.jsx
+++ frontend/src/components/Channels/ChannelItem.jsx
@@ -1,5 +1,6 @@
 import ChannelDropdown from "./ChannelDropdown"
 import Button from 'react-bootstrap/Button'
+import filter from 'leo-profanity'
 
 const ChannelItem = ({ channel, isActive, onSelect, onRename, onDelete }) => {
 
@@ -12,7 +13,7 @@ const ChannelItem = ({ channel, isActive, onSelect, onRename, onDelete }) => {
                     onClick={onSelect}
                 >
                     <span className="me-1" aria-hidden='true'>#</span>
-                    {channel.name} 
+                    {filter.clean(channel.name)} 
                 </Button>
                 {channel.removable && (
                     <ChannelDropdown
@@ -29,3 +30,37 @@ const ChannelItem = ({ channel, isActive, onSelect, onRename, onDelete }) => {
 }
 
 export default ChannelItem
+
+
+
+// import ChannelDropdown from "./ChannelDropdown"
+// import Button from 'react-bootstrap/Button'
+
+// const ChannelItem = ({ channel, isActive, onSelect, onRename, onDelete }) => {
+
+//     return (
+//         <li className="nav-item w-100">
+//             <div className="d-flex align-items-center">
+//                 <Button 
+//                     variant={isActive ? 'secondary' : 'light'}
+//                     className="w-100 rounded-0 text-start text-truncate" // text-truncate для сокращения длинного названия
+//                     onClick={onSelect}
+//                 >
+//                     <span className="me-1" aria-hidden='true'>#</span>
+//                     {channel.name} 
+//                 </Button>
+//                 {channel.removable && (
+//                     <ChannelDropdown
+//                         channelId={channel.id}
+//                         channelName={channel.name}
+//                         onRename={onRename}
+//                         onDelete={onDelete}
+//                         isActive={isActive}
+//                         />
+//                 )}
+//             </div>
+//         </li>
+//     )
+// }
+
+// export default ChannelItem
